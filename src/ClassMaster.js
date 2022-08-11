563e40946dfb9ebe1ef5dbe1712d6e4421b70ec8
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-10 22:28:45 -0500

    Update now a object is pass to the array.

--- src/ClassMaster.js
+++ src/ClassMaster.js
@@ -10,27 +10,34 @@ export default class MyTasksToDo {
 
   addTask(task) {
     const newDiv = document.createElement('div')
-    this.tasks.push(task.value)
+    this.myobject = {};
+    this.myobject.index = this.counter;
+    this.myobject.task = task.value;
+    this.myobject.done = false;
     newDiv.innerHTML = `
         <div class="taskcheck"> <input type="checkbox" class="checkbox" id="task_${this.counter}" name="task_${this.counter}">
         <label class="tasktext" for="task_${this.counter}">${task.value}</label></div><button class="delete"><i class="fa-solid fa-ellipsis-vertical edit"></i></button>`
     task.value = ''
     newDiv.classList.add('task');
     container.appendChild(newDiv);
-    myArray.push(task.value);
+    myArray.push(this.myobject);
     this.counter += 1;
     this.saveStorage()
   }
 
   retriveStorage(oldtask) {
     this.newDiv = document.createElement('div');
+    this.myobject = {};
+    this.myobject.index = this.counter;
+    this.myobject.task = oldtask;
+    this.myobject.done = false;
     this.tasks.push(oldtask);
     this.newDiv.innerHTML = `<div class="taskcheck"> <input type="checkbox" class="checkbox" id="task_${this.counter}" name="task_${this.counter}">
     <label class="tasktext" for="task_${this.counter}">${oldtask}</label></div><button class="delete"><i class="fa-solid fa-ellipsis-vertical edit"></i></button>`
     this.newDiv.classList.add('task');
     container.appendChild(this.newDiv);
     this.counter += 1;
-    myArray.push(oldtask);
+    myArray.push(this.myobject);
   }
 
   deleteTask(task) {
